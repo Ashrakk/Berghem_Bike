df2d89b5be22a1fce2c03ad0ddab1f1e4adbd682
Davide Cuni <prometeoprime@gmail.com>
2020-05-15 01:47:45 +0200

    deleted compiled js and addded *.js to gitignore

--- ts/build/src/ajaxmanager.d.ts
+++
@@ -1,8 +0,0 @@
-export declare class AjaxManager {
-    constructor();
-    ajax_getMapMarkers(callback: Function): void;
-    ajax_check_user_login_status(callback: Function): void;
-    ajax_submit_login(username: string, password: string, callback: Function): void;
-    ajax_submit_logout(callback: Function): void;
-    ajax_submit_reg(username: string, email: string, birth: string, password: string, callback: Function): void;
-}
--- ts/build/src/app.d.ts
+++
@@ -1,3 +0,0 @@
-export { UIManager } from './uimanager.js';
-export { MapsManager } from './mapsmanager.js';
-export { Modal } from './modal.js';
